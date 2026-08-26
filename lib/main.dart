c8c52300d9b8afa16d7fdb3ddf0e43caf7632536
VEGIRAJU MAHAVEER VARMA <86178275+VegirajuMahaveerVarma@users.noreply.github.com>
2026-08-26 21:48:49 +0530

    fix: use corrected home screen in app entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/home_screen_fixed.dart';
 import 'theme/app_theme.dart';
 
-void main(){WidgetsFlutterBinding.ensureInitialized();runApp(const FutureProductivityApp());}
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const FutureProductivityApp());
+}
 
-class FutureProductivityApp extends StatefulWidget{const FutureProductivityApp({super.key});@override State<FutureProductivityApp> createState()=>_AppState();}
-class _AppState extends State<FutureProductivityApp>{bool dark=true;@override Widget build(BuildContext context)=>MaterialApp(debugShowCheckedModeBanner:false,title:'Future Productivity',theme:AppTheme.light(),darkTheme:AppTheme.dark(),themeMode:dark?ThemeMode.dark:ThemeMode.light,home:HomeScreen(dark:dark,onDark:(v)=>setState(()=>dark=v)));}
+class FutureProductivityApp extends StatefulWidget {
+  const FutureProductivityApp({super.key});
+  @override State<FutureProductivityApp> createState() => _AppState();
+}
+
+class _AppState extends State<FutureProductivityApp> {
+  bool dark = true;
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Future Productivity',
+    theme: AppTheme.light(),
+    darkTheme: AppTheme.dark(),
+    themeMode: dark ? ThemeMode.dark : ThemeMode.light,
+    home: HomeScreenFixed(dark: dark, onDark: (v) => setState(() => dark = v)),
+  );
+}
